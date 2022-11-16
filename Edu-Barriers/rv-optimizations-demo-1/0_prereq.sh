93df2f28af5250eff75cff1bf141ea7f879f8105
Sergei Miroshnichenko <s.miroshnichenko@yadro.com>
2022-11-16 17:23:11 +0300

    Pin the versions of Linux, OpenSBI, Buildroot

--- Edu-Barriers/rv-optimizations-demo-1/0_prereq.sh
+++ Edu-Barriers/rv-optimizations-demo-1/0_prereq.sh
@@ -5,9 +5,11 @@
 # Fedora
 sudo dnf install ncurses-devel perl-ExtUtils-MakeMaker ninja-build glib2-devel pixman-devel
 
-git clone git://git.busybox.net/buildroot
+git clone -b 2022.08.x git://git.busybox.net/buildroot
 git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+cd linux; git checkout v6.0; cd ..
 git clone https://github.com/riscv-software-src/opensbi.git
+cd opensbi; git checkout v1.1; cd ..
 git clone https://github.com/qemu/qemu.git
 
 mkdir -p output
