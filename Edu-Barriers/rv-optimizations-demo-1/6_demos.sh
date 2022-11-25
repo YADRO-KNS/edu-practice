2581c387357f9b3702d8d02054f59a76d1bad909
Sergei Miroshnichenko <s.miroshnichenko@yadro.com>
2022-11-25 20:16:51 +0300

    demos: Build with a debug info

--- Edu-Barriers/rv-optimizations-demo-1/6_demos.sh
+++ Edu-Barriers/rv-optimizations-demo-1/6_demos.sh
@@ -4,8 +4,8 @@
 
 mkdir -p ${ROOTFS_OVERLAY}/opt
 
-${CROSS_COMPILE}gcc -O2 demo_races/compiler_opts.c -o $ROOTFS_OVERLAY/opt/compiler_opts
-${CROSS_COMPILE}gcc -O2 demo_races/cpu_opts.c -o $ROOTFS_OVERLAY/opt/cpu_opts
+${CROSS_COMPILE}gcc -O2 -g demo_races/compiler_opts.c -o $ROOTFS_OVERLAY/opt/compiler_opts
+${CROSS_COMPILE}gcc -O2 -g demo_races/cpu_opts.c -o $ROOTFS_OVERLAY/opt/cpu_opts
 
 #make ARCH=riscv CROSS_COMPILE=$CROSS_COMPILE -C linux M=$PWD/demo_races
 #cp demo_races/demo_races.ko $ROOTFS_OVERLAY/opt
