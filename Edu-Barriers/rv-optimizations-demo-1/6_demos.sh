2b1ddf7cf980f124178898bfe70556ae00b5b417
Artyom <arskell221@gmail.com>
2022-11-30 22:01:09 +0300

    Update 6_demos.sh
    missed libpthread link

--- Edu-Barriers/rv-optimizations-demo-1/6_demos.sh
+++ Edu-Barriers/rv-optimizations-demo-1/6_demos.sh
@@ -10,8 +10,8 @@ ${CROSS_COMPILE}gcc -O2 -g demo_races/icache.c -o $ROOTFS_OVERLAY/opt/icache
 
 cp --preserve=mode demo_races/gdbs.sh $ROOTFS_OVERLAY/opt
 
-gcc -O2 -g demo_races/icache.c -o output/icache
-gcc -O2 -g demo_races/cpu_opts.c -o output/cpu_opts
+gcc -O2 -g -pthread demo_races/icache.c -o output/icache
+gcc -O2 -g -pthread demo_races/cpu_opts.c -o output/cpu_opts
 
 #make ARCH=riscv CROSS_COMPILE=$CROSS_COMPILE -C linux M=$PWD/demo_races
 #cp demo_races/demo_races.ko $ROOTFS_OVERLAY/opt
